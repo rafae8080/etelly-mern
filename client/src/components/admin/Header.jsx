ae8dfc27f10e02cacb82c0898767012cce3d52e7
Rafael Marinas <work.rafael22@gmail.com>
2026-05-05 03:02:58 +0800

    header

--- client/src/components/admin/Header.jsx
+++ client/src/components/admin/Header.jsx
@@ -30,7 +30,7 @@ export default function Header({ userEmail = "", onLogout }) {
 
   return (
     <>
-      <header className="bg-white border-b border-gray-200 px-6 py-4">
+      <header className="bg-white border-b border-gray-200 pl-14 pr-6 py-4 lg:px-6">
         <div className="flex items-center justify-between">
           <div className="flex-1 max-w-md" />
 
